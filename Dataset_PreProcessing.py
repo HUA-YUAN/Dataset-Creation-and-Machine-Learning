fd0d1850906a615bc2e14fe91ef24d3355208bde
HUA-YUAN <stanlee0325@gmail.com>
2025-05-09 17:06:26 +0800

    Add files via upload

--- Dataset_PreProcessing.py
+++ Dataset_PreProcessing.py
@@ -8,7 +8,13 @@ Created on Thu May  1 13:45:01 2025
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+import scipy.integrate as spi
 
+
+
+plt.style.use("ggplot")
+plt.rcParams['font.sans-serif'] = ['Microsoft Yahei']
+plt.rcParams['axes.unicode_minus'] = False
 
 
 # 判斷P波到達時間   
@@ -17,30 +23,19 @@ def automatically_picking_p_time(a, dt, n, sta, lta, p_value):
     n_sta = int(sta/dt)
     n_lta = int(lta/dt)
     a1 = abs(a)
-    
-    a_sta = []
-    a_lta = []
-    r = []
-    
-    for i in range(0,n_sta):
-            a_sta.insert(i,0)
-            
-    for i in range(0,n_lta):
-            a_lta.insert(i,0)
-            r.insert(i,0)
-            
+
+    a_sta = [0] * n  
     for i in range(n_sta,n):           
-            a_sta1 = (sum(a1[i-n_sta:i-1]))/n_sta
-            a_sta.append(a_sta1)
-            
+            a_sta[i] = (sum(a1[i-n_sta:i-1]))/n_sta
+
+    a_lta = [0] * n  
     for i in range(n_lta,n):          
-            a_lta1 = (sum(a1[i-n_lta:i-1]))/n_lta
-            a_lta.append(a_lta1)
-    
+            a_lta[i] = (sum(a1[i-n_lta:i-1]))/n_lta
+
+    r = [0] * n  
     for i in range(n_lta,n):
-            r1 = a_sta[i]/a_lta[i]
-            r.append(r1)  
-            
+            r[i] = a_sta[i]/a_lta[i]
+
     for i in range(n_lta,n): 
         if r[i] >= p_value:
             p_arrive = i*dt
@@ -77,15 +72,12 @@ def draw_time_sta_lta(t,a_sta,a_lta,r):
     #plt.savefig(plt_name)
     plt.show()
     
-    
-# 繪製P波加速度圖與比值變化圖
-def draw_p_arrive_test(t,p_arrive,a_u,r):
-    # 設定nrows(垂直)、ncols(水平)和dpi(解析度)
+
+def draw_p_arrive_test(t, p_arrive, a_u, r):
     fig, ax = plt.subplots(2,1,dpi=150, figsize = (8,4.5))
-    # ax[i]中的i為index(位置)參數
-    ax[0].set_title('P波加速度圖與比值變化圖')
+    ax[0].set_title('U-D acceleration and STA/LTA ratio')
     ax[0].plot(t,a_u)
-    ax[0].set_ylabel(r"加速度")
+    ax[0].set_ylabel(r"U-D acceleration")
     ax[0].grid(True)
     ax[0].set_xlim(0,t[-1])
     ax[0].axvline(x=p_arrive, ymin=0, ymax=3,color="blue")
@@ -96,7 +88,7 @@ def draw_p_arrive_test(t,p_arrive,a_u,r):
     ax[1].set_xlim(0,t[-1])
     ax[1].axvline(x=p_arrive, ymin=0, ymax=3,color="blue")
     
-    plt.xlabel(r"時間 (s)")
+    plt.xlabel(r"time (s)")
     plt.tight_layout()
     #plt_name = j.split("/")[-1].replace(".txt","") + "_p" + str(p_arrive) + ".png"
     #plt.savefig(plt_name)
@@ -106,30 +98,56 @@ def draw_p_arrive_test(t,p_arrive,a_u,r):
 
 # 將加速度值進行積分取得速度與位移值
 def integrate_acceleration(a, dt, n):
-    v = []
-    d = []
-    cav = []
+    v = [0] * n
+    v0 = 0
+    v1 = spi.cumtrapz(a,dx=dt,initial=v0)
+    for i in range(1, n):
+        v[i] = v1[i]-v1[i-1]
     
-    # v = v0 + a*dt
-    v = a*dt
-    # d = d0 + v*dt
-    d = v*dt
+    d = [0] * n
+    d0=0
+    d1 = spi.cumtrapz(v,dx=dt,initial=d0)  
+    for i in range(1, n):
+        d[i] = d1[i]-d1[i-1]
     
-    cav0 = 0
-    cav.append(cav0)
-    cav2 = []
-    for i in range(0,n-1):           
-           cav1 = abs(v[i+1] - v[i])
-           cav2.append(cav1)
-           
+    cav = [0] * n
+    cav1 = [0] * n
+    for i in range(1,n):           
+           cav1[i] = abs(v[i] - v[i-1])
     for i in range(1,n):  
-           cav3 = sum(cav2[0:i])
-           cav.append(cav3)
-                   
+           cav[i] = sum(cav1[0:i])
+           
     return v, d, cav
 
 
-
+# 繪製STA、LTA與比值變化圖 
+def draw_time_a_v_t(t,a,v,cav): 
+    # 設定nrows(垂直)、ncols(水平)和dpi(解析度)
+    fig, ax = plt.subplots(3,1,dpi=150,figsize = (8,6))
+    # ax[i]中的i為index(位置)參數
+    ax[0].set_title('STA、LTA與比值變化圖')
+    ax[0].plot(t,a)
+    ax[0].set_ylabel(r"STA")
+    ax[0].grid(True)
+    ax[0].set_xlim(0,t[-1])
+   
+    ax[1].plot(t,v)
+    ax[1].set_ylabel(r"LTA")
+    ax[1].grid(True)
+    ax[1].set_xlim(0,t[-1])
+    
+    ax[2].plot(t,cav)
+    ax[2].set_ylabel(r"STA/LTA")
+    ax[2].grid(True)
+    ax[2].set_xlim(0,t[-1])
+   
+    plt.xlabel(r"時間 (s)")
+    plt.tight_layout()
+    # 將圖片命名並儲存為.png檔
+    #plt_name = j.split("/")[-1].replace(".txt","") + "_p" + str(p_arrive) + ".png"
+    #plt.savefig(plt_name)
+    plt.show()
+    
 
 def filter_wave(x, dt, fl, fh, btype, order=2):
     fn = 1.0/dt/2.0  # 奈奎斯特頻率
@@ -166,4 +184,5 @@ def calculate_feature(dt, a, p_arrive, v_f, d_f, cav):
     data_STD = np.std(a[eigenvalue_num:arrive_after3-1])
 
     
-    return data_Pa, data_Pv, data_Pd, data_CAV_p, data_Max, data_Min, data_P2P, data_Abs_Mean, data_STD
+    return data_Pa, data_Pv, data_Pd, data_CAV_p,\
+           data_Max, data_Min, data_P2P, data_Abs_Mean, data_STD
